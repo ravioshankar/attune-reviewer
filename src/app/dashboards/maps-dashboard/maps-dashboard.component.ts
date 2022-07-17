371a961f8c4c4321502de16a020177f811b2b9f5
Ravi Shankar <ravioshankar@outlook.com>
2022-07-17 14:46:20 -0500

    Map dashboard card update  (#2)
    * map dashboard
    
    * refreshed map dashboard

--- src/app/dashboards/maps-dashboard/maps-dashboard.component.ts
+++ src/app/dashboards/maps-dashboard/maps-dashboard.component.ts
@@ -3,6 +3,15 @@ import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import homePageData from 'src/assets/data/homePageData.json';
 import { iHomePageData } from 'src/app/interface/home-page-data.interface';
+import { fromLonLat } from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import { Feature, View } from 'ol';
+import { Point } from 'ol/geom';
+import VectorLayer from 'ol/layer/Vector';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import Map from 'ol/Map';
 
 @Component({
   selector: 'app-maps-dashboard',
@@ -10,27 +19,75 @@ import { iHomePageData } from 'src/app/interface/home-page-data.interface';
   styleUrls: ['./maps-dashboard.component.scss'],
 })
 export class MapsDashboardComponent {
-  homePageMetaData: iHomePageData = homePageData;
+  homePageMetaData: iHomePageData = structuredClone(homePageData);
+
+  map!: Map;
+
+  currentLocation: any;
+
+  ngOnInit() {
+    this.initilizeMap();
+  }
+
+  initilizeMap() {
+    navigator.geolocation.getCurrentPosition((p) => {
+      this.currentLocation = fromLonLat([
+        p.coords.longitude,
+        p.coords.latitude,
+      ]);
+
+      const raster = new TileLayer({
+        source: new OSM(),
+      });
+
+      var source = new VectorSource({
+        features: [new Feature(new Point(this.currentLocation))],
+      });
+      var vector = new VectorLayer({
+        source: source,
+        style: new Style({
+          fill: new Fill({
+            color: 'rgba(255, 255, 255, 0.2)',
+          }),
+          stroke: new Stroke({
+            color: '#ffcc33',
+            width: 2,
+          }),
+          image: new CircleStyle({
+            radius: 7,
+            fill: new Fill({
+              color: 'red',
+            }),
+          }),
+        }),
+      });
+      this.map = new Map({
+        target: 'map',
+        layers: [raster, vector],
+        view: new View({
+          center: this.currentLocation,
+          zoom: 12,
+        }),
+      });
+    });
+  }
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
-        ];
+        return Object.values(this.homePageMetaData);
       }
+      return Object.values(this.homePageMetaData);
 
-      return [
-        { title: 'Card 1', cols: 1, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 1 },
-        { title: 'Card 4', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 1 },
-        { title: 'Card 4', cols: 1, rows: 1 },
-      ];
+      // return [
+      //   { title: 'Openlayers', cols: 1, rows: 1 },
+      //   { title: 'Card 2', cols: 1, rows: 1 },
+      //   { title: 'Card 3', cols: 1, rows: 1 },
+      //   { title: 'Card 4', cols: 1, rows: 1 },
+      //   { title: 'Card 3', cols: 1, rows: 1 },
+      //   { title: 'Card 4', cols: 1, rows: 1 },
+      // ];
     })
   );
 
