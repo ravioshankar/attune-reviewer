fcaffdf02b31809f96ad4a3d2e325f6a41cc7c32
Ravi Shankar <ravioshankar@outlook.com>
2022-07-17 15:24:17 -0500

    map card

--- src/app/dashboards/home-dashboard/home-dashboard.component.ts
+++ src/app/dashboards/home-dashboard/home-dashboard.component.ts
@@ -13,18 +13,16 @@ export class HomeDashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
+          { title: 'Card 1', cols: 1, rows: 1, id: 1 },
+          { title: 'Card 2', cols: 1, rows: 1, id: 2 },
+          { title: 'Card 3', cols: 1, rows: 1, id: 3 },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 },
+        { title: 'Card 1', cols: 2, rows: 1, id: 1 },
+        { title: 'Card 2', cols: 1, rows: 1, id: 2 },
+        { title: 'Card 3', cols: 1, rows: 1, id: 3 },
       ];
     })
   );
